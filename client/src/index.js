80e7c520bec1853ac3f8847856396c4441938111
Erik McComb <erik@erikmccomb.com>
2017-09-20 10:08:01 -0600

    store, corrections

--- client/src/index.js
+++ client/src/index.js
@@ -6,17 +6,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter } from 'react-router-dom';
-import {provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import store from './store';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-  document.detElementById('root')
-);
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
