a6f664b26c5b0b8ecb0856f1d8da24eab3715a08
Erik McComb <erik@erikmccomb.com>
2017-09-20 10:06:30 -0600

    index filled. store.js created

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
+// Main app. this is the only component mounted on pageXOffset. 
+// it has children that create teh experience
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter } from 'react-router-dom';
+import {provider} from 'react-redux';
+import store from './store';
+import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  document.detElementById('root')
+);
 registerServiceWorker();
